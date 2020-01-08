ef67e2a4e1d655ca25bb7d1f58f724094ec60cc4
josephsdavid <josephsd@smu.edu>
2020-01-08 11:46:06 -0600

    made deeper LSTM

--- lstm/deeper_lstm.py
+++ lstm/deeper_lstm.py
@@ -1,7 +1,7 @@
 import numpy as np
 from load_pretrain import read_data, read_data_unrolled
 from keras.layers import Dense, Dropout, LSTM, Input
-from keras.layers import Embedding, Activation
+from keras.layers import Embedding, Activation, BatchNormalization
 from keras.models import Model
 from keras.utils import to_categorical
 from keras.utils.generic_utils import get_custom_objects
@@ -12,19 +12,24 @@ import matplotlib.pyplot as plt
 X, y = read_data("../PreTrainingDataset")
 y = to_categorical(y)
 
+X.shape
+
 
 
 # build the simplest lstm possible first
 # then maybe stateful who knows
 # try out embedding layers too
-class simple_lstm_classifier:
-    def __init__(self, X, y, act = 'relu', dropout = False, stateful = False):
+class lstm_classifier:
+    def __init__(self, X, y, act = 'relu', dropout = True, stateful = False):
         n_timesteps, n_features, n_outputs = X.shape[1], X.shape[2], y.shape[1]
         start = Input((None, n_features), name = 'Input')
-        x = LSTM(8, activation = act, name = 'LSTM_1', stateful = stateful)(start)
-        if (dropout):
+        x = LSTM(8, activation = act, name = 'LSTM_1', return_sequences = True, dropout = 0.5, recurrent_dropout = 0.5)(start)
+        x = LSTM(16, activation = act, name = 'LSTM_2', dropout = 0.5, recurrent_dropout = 0.5)(x)
+        for i in range(3):
             x = Dropout(0.5)(x)
-        x = Dense(n_outputs, activation = 'relu')(x)
+            x = Dense(32, activation = act)(x)
+        x = Dropout(0.5)(x)
+        x = Dense(n_outputs, activation = 'relu', name = 'output')(x)
         self.model = Model(start, x)
 
     def fit(self, X, y,
@@ -43,24 +48,5 @@ class simple_lstm_classifier:
         self.history = self.model.fit(X, y, **fit_options)
 
 
-lstm = simple_lstm_classifier(X, y, dropout = True)
+lstm = lstm_classifier(X, y)
 lstm.fit(X,y)
-
-
-history = lstm.history
-
-
-plt.plot(history.history['accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-
-# summarize history for loss
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-plt.show()
