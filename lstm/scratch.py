3e1e0b7a3ce15e42e6152088d69e46dda121ac05
josephsdavid <josephsd@smu.edu>
2020-01-12 12:46:57 -0600

    running data grid search

--- lstm/scratch.py
+++ lstm/scratch.py
@@ -17,14 +17,52 @@ from keras.preprocessing.sequence import TimeseriesGenerator
 import matplotlib.pyplot as plt
 import itertools as it
 
+X_test, y_test = test_loader("../EvaluationDataset")
+y_test = to_categorical(y_test)
+
+class simple_lstm_classifier:
+    def __init__(self, X, y, act = 'tanh', dropout = 0, stateful = False):
+        n_timesteps, n_features, n_outputs = X.shape[1], X.shape[2], y.shape[1]
+        start = Input((None, n_features), name = 'Input')
+        x = LSTM(20, activation = act, name = 'LSTM_1', stateful = stateful, dropout = dropout, recurrent_dropout = dropout)(start)
+        out = Dense(n_outputs, activation = 'softmax')(x)
+        self.model = Model(start, out)
+
+    def fit(self, X, y, lr = .001,
+            compilation_options = {
+                'loss' : 'categorical_crossentropy',
+                'metrics' : ['accuracy']},
+            fit_options = {
+                'epochs': 100,
+                'batch_size':400,
+                'validation_split':0.33,
+                'shuffle':False,
+                'verbose':1
+            }):
+        adam = optimizers.adam(lr = lr)
+        self.model.compile(optimizer = adam, **compilation_options)
+        self.history = self.model.fit(X, y, **fit_options)
+
+
+fit_options = {
+    'epochs': 50,
+    'batch_size':400,
+    'shuffle':True,
+    'verbose':1
+}
 
 # thankful for grid search homework
 pars = ["scale","noise","filter"]
 tunables = {key: [True, False] for key in pars}
-grid = list(it.product(*((tunables[i]) for i in tunables)))
+grid = list(it.product(*(tunables[i] for i in tunables)))
 
+results = {}
+for i in range(len(grid)):
+    hp = dict(zip(pars, grid[i]))
+    X, y = read_data_augmented("../PreTrainingDataset", **hp)
+    lstm = simple_lstm_classifier(X,y, dropout = 0.1)
+    lstm.fit(X, y, lr = 0.0005, fit_options=fit_options)
+    score=lstm.model.evaluate(X_test, y_test)[1]
+    results[grid[i]] = score
 
-
-
-
-import itertools
+results
