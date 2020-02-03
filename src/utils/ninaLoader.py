75ae18510a69a178df70268a3280b571d3a7bb67
Carson Drake <drakec@smu.edu>
2020-02-02 19:28:21 -0800

    idk whats happening anymore

--- src/utils/ninaLoader.py
+++ src/utils/ninaLoader.py
@@ -7,7 +7,7 @@ import argparse
 from .helpers import read_file_validation, pad_along_axis
 from .augmentors import window_roll, roll_labels, add_noise
 from .loaders import Loader
-from .preprocessors import butter_highpass_filter
+from .preprocessors import butter_highpass_filter, scale
 
 parser = argparse.ArgumentParser(description='Config loader...')
 parser.add_argument('-v', dest='verbose', action='store_true', default=False,
@@ -23,30 +23,30 @@ parser.add_argument('-p','--path', dest='nPath', action='store', default="./../d
 args = parser.parse_args()
 
 VERBOSE = args.verbose
-MAX_SEQ = args.maxSize
-EXCERCISE = list(args.excercise.strip())
-NINA_BASE = args.nPath
+MAX_SEQ = int(args.maxSize)
+EXCERCISE = list(str(args.excercise).strip())
+NINA_BASE = str(args.nPath)
 # This function returns two lists. basically groups on subject, then exercise
 # data: list of lists of 2D matrix (default emg data only)
 # labs: list of lists of arrays of which exercise trial 
-def _load_by_subjects_raw(nina_path=".", subjects=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], options=None):
-    data = []
-    labs = []
-    if type(subjects) is int:
-        subs = [subjects]
-    else:
-        subs = subjects
-    for sub in subs:
-        subData = []
-        subLabs = []
-        for i in range(1,4):
-            path = nina_path + "/s" + str(sub) + "/S" + str(sub) + "_E" + str(i) + "_A1.mat"
-            fileData, l = _load_file(path, options)
-            subData.append(fileData)
-            subLabs+=l
-        data.append(subData)
-        labs+= subLabs
-    return data, labs
+# def _load_by_subjects_raw(nina_path=".", subjects=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], options=None):
+#     data = []
+#     labs = []
+#     if type(subjects) is int:
+#         subs = [subjects]
+#     else:
+#         subs = subjects
+#     for sub in subs[:3]:
+#         subData = []
+#         subLabs = []
+#         for i in range(1,4):
+#             path = f"{nina_path}/s{str(sub)}/S{str(sub)}_E{str(i)}_A1.mat"
+#             fileData, l = _load_file(path, options)
+#             subData.append(fileData)
+#             subLabs+=l
+#         data.append(subData)
+#         labs+= subLabs
+#     return data, labs
 
 
 class NinaLoader(Loader):
@@ -57,16 +57,25 @@ class NinaLoader(Loader):
             self.excercises = [excercises]
         self.processors = process_fns
         self.augmentors = augment_fns
-        self.read_data()
+        self.emg = self.read_data()
+        if VERBOSE : 
+            print(f"[Step 1 ==> processing] Shape of emg: {np.shape(self.emg.copy())}")
+            print(f"[Step 1 ==> processing] Shape of labels: {np.shape(self.labels.copy())}")
         self.process_data()
         if VERBOSE : 
-            print(f"Shape of emg: {np.shape(self.emg)}")
-            print(f"Shape of labels: {np.shape(self.labels)}")
+            print(f"[Step 2 ==> augment] Shape of emg: {np.shape(self.emg)}")
+            print(f"[Step 2 ==> augment] Shape of labels: {np.shape(self.labels)}")
         if augment_fns is not None:
             self.augment_data(step, window_size)
+        if VERBOSE : 
+            print(f"[Step 3 ==> moveaxis] Shape of emg: {np.shape(self.emg)}")
+            print(f"[Step 3 ==> moveaxis] Shape of labels: {np.shape(self.labels)}")
         self.emg = np.moveaxis(np.concatenate(self.emg,axis=0),2,1)
+        if VERBOSE : 
+            print(f"[Step 4 ==> scale] Shape of emg: {np.shape(self.emg)}")
+            print(f"[Step 4 ==> scale] Shape of labels: {np.shape(self.labels)}")
         if scale:
-            self.emg = pp.scale(self.emg)
+            self.emg = scale(self.emg)
     
     # features can be an array if we need to pass back additional 
     # features with the emg data. could help recycle this
@@ -76,12 +85,13 @@ class NinaLoader(Loader):
         data = []
         # Might need to start clipping emg segments here... RAM is 
         # struggling to keep up with massive sizes
-        emg = res['emg'][:,:8].copy()
-        lab = res['restimulus'][:].copy()
-        del res
+        emg = res['emg'][:MAX_SEQ,:8].copy()
+        lab = res['restimulus'][:MAX_SEQ].copy()
+        
         data.append(emg)
         if features:
             for ft in features:
+                VERBOSE and print('adding features')
                 sameDim = data[0].shape[0]==np.shape(res[ft])[0]
                 newData = []
                 if not sameDim and np.shape(res[ft])[1]==1:
@@ -89,6 +99,8 @@ class NinaLoader(Loader):
                 else:
                     newData = res[ft]
                 data.append(newData)
+
+        del res
         return np.concatenate(data,axis=1), lab
 
     def _load_by_trial_raw(self, trial=1, options=None):
@@ -96,7 +108,7 @@ class NinaLoader(Loader):
         labs = []
         reps = []
         for i in range(1,11):
-            print(f"Starting load of {i}/10 .mat files")
+            VERBOSE and print(f"Starting load of {i}/10 .mat files")
             path = self.path + "/" + "s" + str(i) + "/S" + str(i) + "_E" + str(trial) + "_A1.mat"
             fileData, l = self._load_file(path, options)
             data.append(fileData)
@@ -123,14 +135,15 @@ class NinaLoader(Loader):
             exData, l = self._load_by_trial_raw(trial=e)
             res+=exData
             labels+=l
-        return np.concatenate(res), np.concatenate(labels)
+            VERBOSE and print(f"[Step 0] \nexData {np.shape(exData.copy())}\nlabels {np.shape(labels.copy())}")
+        return np.hstack(res), np.hstack(labels)
 
 
     
 
     def read_data(self):
-        self.emg, self.labels = self._read_group_to_lists()
-        self.emg = [pad_along_axis(x, 1000) for x in self.emg]
+        _emg, self.labels = self._read_group_to_lists()
+        return [pad_along_axis(x, 1000) for x in _emg]
 
     def process_data(self):
         for f in self.processors:
