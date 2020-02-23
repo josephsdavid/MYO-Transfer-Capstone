1b77ba30a0e779d1ac4f179b97f68178f403e085
Carson Drake <drakec@smu.edu>
2020-02-22 19:11:15 -0800

    make shell script better. makes np directory if it doesn'

--- src/mat_to_numpy.sh
+++ src/mat_to_numpy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+
+directory_name="../data/ninaPro/np"
+
+if [[ -d "${directory_name}" ]]; then
+    echo "Directory already exists"
+else
+    echo "Making target np directory..."
+    mkdir "${directory_name}"
+fi
+echo "Generating npy files..."
 python -c 'from utils import ninaUtils as nu; nu.build_nps("../data/ninaPro")'
+# For 16-channel data
+# python -c 'from utils import ninaUtils as nu; nu.build_nps("../data/ninaPro", prefix="emg16", full_emg=True)'
+echo "El Fin!"
