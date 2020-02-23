481226aafba74f3024e688e80e4025fcd6fe63c9
josephsdavid <josephsd@smu.edu>
2020-02-23 08:55:07 -0600

    ran a simple test with small net

--- src/experiment.py
+++ src/experiment.py
@@ -7,7 +7,11 @@ from tensorflow.keras.initializers import Constant
 from tensorflow.keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from tensorflow.keras.models import Model
 from tensorflow.keras.utils import plot_model, to_categorical
-from builders.recurrent import build_att_gru_norm, build_att_gru
+from tensorflow.keras.layers import Add, Input, Dense, GRU
+#from builders.recurrent import build_att_gru_norm, build_att_gru
+from activations import Mish
+from optimizers import Ranger
+from layers import Attention, LayerNormalization
 batch=512
 
 
@@ -27,12 +31,59 @@ test = u.NinaMA("../data/ninaPro", ['b'], [u.butter_highpass_filter],
 n_time = train[0][0].shape[1]
 n_class =train[0][1].shape[-1]
 
-print("n_timesteps{}".format(n_time))
+print("n_timesteps: {}".format(n_time))
 
 neg = Constant(value=-1)
 
+def gru(inn, nodes=40, **kwargs):
+    return GRU(nodes, activation=Mish(),  return_state=True, return_sequences=True, reset_after=True, recurrent_activation='sigmoid')(inn, **kwargs)
+
+
+def block(inn, nodes=40,**kwargs):
+    val, state = gru(inn, nodes,**kwargs)
+    val2 = Attention()(val)
+    return val, state, val2
+
+
+def build_att_gru(n_time, n_classes, nodes=40, blocks=3,
+                  loss='categorical_crossentropy', optimizer=Ranger,model_id=None, **optim_args):
+    '''
+    build_att_gru
+    --------------
+    args:
+        n_time
+        n_out
+        nodes=40
+        blocks=3, represents number of att_gru blocks in the model
+        loss='categorical_crossentropy'
+        optimizer=Ranger (no parentheses)
+        model_id = model id
+        args for optimizer
+    Notes:
+        returns compiled model.
+        requires by default one hot encoded Y data
+    '''
+    inputs = Input((n_time, 8))
+    x = Dense(128)(inputs)
+    x, h, a = block(x, nodes)
+    attention=[a]
+    for _ in range(blocks-1):
+        x, h, a = block(x, nodes, initial_state=h)
+        attention.append(a)
+    out = Add()(attention)
+    outputs = Dense(n_classes, activation="softmax")(out)
+    model = Model(inputs, outputs)
+    model.compile(optimizer(**optim_args), loss=loss,  metrics=['accuracy'])
+    print(model.summary())
+    if model_id is not None:
+        model.load_weights(f"{model_id}.h5")
+    return model
+
+
+
 model=build_att_gru(n_time, n_class)
 tf.keras.utils.plot_model(model, to_file="attn.png", show_shapes=True, expand_nested=True)
+
 #model.compile(Ranger(), loss='categorical_crossentropy', metrics=['accuracy'])
 class_weights = {i:1/(n_class) if i==0 else 1 for i in range(1, n_class+1)}
 h2 = model.fit(train, epochs=100, validation_data=test, shuffle=False,
@@ -40,22 +91,23 @@ h2 = model.fit(train, epochs=100, validation_data=test, shuffle=False,
                class_weight=class_weights)
 
 
-plt.subplot(212)
-plt.plot(h2.history['accuracy'])
-plt.plot(h2.history['val_accuracy'])
-plt.title('model accuracy')
-plt.ylabel('accuracy')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-# summarize history for loss
-plt.subplot(211)
-plt.plot(h2.history['loss'])
-plt.plot(h2.history['val_loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(['train', 'test'], loc='upper left')
-F = plt.gcf()
-Size = F.get_size_inches()
-F.set_size_inches(Size[0]*2, Size[1]*2)
-plt.savefig("simple_lstm_training.png")
+#import matplotlib.pyplot as plt
+#plt.subplot(212)
+#plt.plot(h2.history['accuracy'])
+#plt.plot(h2.history['val_accuracy'])
+#plt.title('model accuracy')
+#plt.ylabel('accuracy')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'test'], loc='upper left')
+## summarize history for loss
+#plt.subplot(211)
+#plt.plot(h2.history['loss'])
+#plt.plot(h2.history['val_loss'])
+#plt.title('model loss')
+#plt.ylabel('loss')
+#plt.xlabel('epoch')
+#plt.legend(['train', 'test'], loc='upper left')
+#F = plt.gcf()
+#Size = F.get_size_inches()
+#F.set_size_inches(Size[0]*2, Size[1]*2)
+#plt.savefig("lstm_untuned.png")
